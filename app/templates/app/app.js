c0c6e68ac35fd5bfc1b63753cc24fb97c1f8c4f3
moldyman <erik.moldovan@door3.com>
2014-04-21 15:24:51 -0400

    Re-adding in module definition JSON

--- app/templates/app/app.js
+++ app/templates/app/app.js
@@ -2,10 +2,12 @@ define([
         'underscore', 
         'jquery', 
         'backbone', 
-        'marionette'
+        'marionette',
+        'config/regions/dialog',
+        'json!fixtures/modules.json'
     ],
 
-    function( _, $, Backbone, Marionette ) {
+    function( _, $, Backbone, Marionette, DialogRegion, modulesList ) {
         'use strict';
 
         var App = new Marionette.Application();
@@ -22,16 +24,16 @@ define([
         // Initializer Handler
         App.on('initialize:after', function(){
             // Pulls in the list of modules dynamically from JSON, as filtered by permissions
-            // var modulesArray = [];
+            var modulesArray = [];
 
-            // _.each(modulesList, function(module){
-            //     modulesArray.push(module.path);
-            // });
+            _.each(modulesList, function(module){
+                modulesArray.push(module.path);
+            });
 
-            // require(modulesArray, function(){
+            require(modulesArray, function(){
                 // Kick off Backbone.history to resolve current url
                 Backbone.history.start({ pushState: true, root: '/' });
-            // }); 
+            }); 
         });
 
         return App;
