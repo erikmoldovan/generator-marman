23e7ec317b3cf5b9b7b21ea24583a4866c67cbfa
moldyman <erik.moldovan@door3.com>
2014-04-22 21:33:18 -0400

    Unifying the Router and Controller in a sensible way. Will work on dynamic app-level route loading (depends on App.ModuleManager)

--- app/templates/app/global/app.router.js
+++ app/templates/app/global/app.router.js
@@ -6,33 +6,21 @@ define([
   function(Backbone, Marionette){
     'use strict';
 
-    // var Controller = Marionette.Controller.extend({
-    //   loadTest1: function(){
-    //     console.log('test 1 route loaded');
-    //   },
-
-    //   loadTest2: function(){
-    //     console.log('test 2 route loaded');
-    //   }
-    // });
-
-    var Controller = {
-      loadTest1: function(){
-        console.log('test 1 route loaded');
-      },
-
-      loadTest2: function(){
-        console.log('test 2 route loaded');
-      }
-    };
-
     var Router = Marionette.AppRouter.extend({
-      controller: Controller,
       appRoutes: {
         "*defaults": "loadTest1",
         "test1": "loadTest1",
         "test2": "loadTest2"
       },
+      controller: {
+        loadTest1: function(){
+          console.log('test 1 route loaded');
+        },
+
+        loadTest2: function(){
+          console.log('test 2 route loaded');
+        }
+      }
     });
 
     return Router;
